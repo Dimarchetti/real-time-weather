6cc0d360f28b86585d739763c308e499797a4c74
Dimitri Marchetti <dimitri.marchetti@gmail.com>
2021-10-13 11:57:06 -0300

    hiding api keys

--- real-time-weather.py
+++ real-time-weather.py
@@ -3,7 +3,7 @@ import json
 
 cidade = input('Qual o nome da sua Cidade? ')
 
-req = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+cidade+'&appid=65b38c99811f47c1e117b2f4bcc87ca6')
+req = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+cidade+'&appid=xxxxxxxxxxxxxxxxxxxxxxxxxx')
 
 tempo = json.loads(req.text)
 cond_tempo = tempo['weather'][0]['main']
